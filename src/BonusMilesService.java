0201d40cf38092e535dd33c862a8ff0fc53fa04a
GorLena <zhura1977@list.ru>
2024-07-02 19:02:16 +0300

    README.md

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    int calculate(int price) {
+        int miles;
+        miles = 20;
+        int result;
+        result = price / miles;
+        return result;
+    }
+}
